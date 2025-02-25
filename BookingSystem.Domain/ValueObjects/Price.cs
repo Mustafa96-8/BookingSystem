dd6dfeaeeda798265a159ca6895d63d05e05c650
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-02-25 23:29:43 +0500

    Добавил объект стоимости, хранит в себе величину и валюту

---
+++ BookingSystem.Domain/ValueObjects/Price.cs
@@ -0,0 +1,26 @@
+﻿using BookingSystem.Domain.Common;
+using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.ValueObjects;
+
+public record Price
+{
+    public Price(double value,string currency)
+    {
+        this.value = value;
+        this.currency = currency;
+    }
+    public double value { get; private set; }
+    public string currency { get; private set; }
+
+    //TO DO Добавить валидацию
+    public static Result<Price,Error> Create(double value,string currency)
+    {
+        return new Price(value, currency);
+    }
+}
