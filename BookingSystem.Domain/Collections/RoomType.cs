4f8a8161d51d43b455f2852c42735e9e269ffcb2
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-02-25 21:40:09 +0500

    Добавлены коллекции типов комнат и удобств

---
+++ BookingSystem.Domain/Collections/RoomType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.Collections;
+/// <summary>
+/// Перечисление возможных типов номеров в отелях (Одноместный, Двухместный с одной кроватью, С двумя кроватями...)
+/// </summary>
+public enum RoomType
+{
+    /// <summary>
+    /// Одноместный 
+    /// </summary>
+    Single,
+    /// <summary>
+    /// Двухместный с одной кроватью
+    /// </summary>
+    Double,
+    /// <summary>
+    /// Двухместный с двумя кроватями
+    /// </summary>
+    Twin,
+    /// <summary>
+    /// Трехместный 
+    /// </summary>
+    Triple,
+    /// <summary>
+    /// Четырехместный 
+    /// </summary>
+    Quadriple,
+
+}
---
+++ BookingSystem.Domain/Collections/Сonvenience.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.Collections;
+
+/// <summary>
+/// Перечисление возможных удобств в номере
+/// </summary>
+public enum Сonvenience
+{
+    WiFi,
+    Kitchen,
+    Teapot,
+    Hairdryer,
+    Jacuzzi,
+    MiniBar,
+    RoomService,
+    WithDisabilities
+}
