82fd01681b315711b59b13ea8df085a671a8d729
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-03-19 13:22:31 +0500

    Добавлено расширение для удобной проверки пустого значения строки

---
+++ BookingSystem.Domain/Common/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Domain.Common
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
+    }
+}
