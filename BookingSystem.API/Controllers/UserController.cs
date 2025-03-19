1521b650a639b30202d0a0a450fae90dfaeeea6f
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-03-19 20:51:19 +0500

    Реализован контроллер пользователя с операциями Create Getall getById

--- BookingSystem.API/Controllers/UserController.cs
+++ BookingSystem.API/Controllers/UserController.cs
@@ -1,15 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookingSystem.Application.Services;
+using Contracts.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingSystem.API.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class UserController : ControllerBase
+public class UserController : BaseController
 {
+    private readonly UserService _userService;
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
 
+    // GET: api/<UserController>
+    [HttpGet]
+    public async Task<IActionResult> Get(CancellationToken ct)
+    {
+        var result = await _userService.GetAll(ct);
 
+        if(result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
+        return BadRequest(result.Error);
 
+    }
+    // </snippet_GetByID>
 
-    
+    // GET: api/<UserController>/<guid>
+    // <snippet_Update>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid? id, CancellationToken ct)
+    {
+        var result = await _userService.GetById(id, ct);
+        if(result.IsFailure)
+            return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+    // </snippet_Update>
+
+    // POST: api/<UserController>
+    // <snippet_Create>
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] UserDTO request, CancellationToken ct)
+    {
+        var result = await _userService.Create(request, ct);
+        if(result.IsFailure)
+            return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+    // </snippet_Create>
 
 }
